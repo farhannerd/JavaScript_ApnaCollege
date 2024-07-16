b75721f2677442ecf99375d2a69fc11996aafc20
Farhan Akhtar <gitfarhan@gmail.com>
2024-07-16 12:49:19 +0530

    heheh

---
+++ JS 5 - Function & Methods/script.js
@@ -0,0 +1,17 @@
+console.log("Hello");
+"abc".toUpperCase;
+[1, 2, 3].push(4);
+// Ye teeno functions hain
+
+/* 
+function functionName(){
+    // do some work
+}
+*/
+
+function myfunc() {
+    console.log("Farhan Akhtar");
+}
+
+myfunc();
+myfunc();

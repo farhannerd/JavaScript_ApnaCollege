b6f7ace8f48823dc30b9171d3a977fde85e1a4bb
Farhan Akhtar <gitfarhan@gmail.com>
2024-07-18 17:36:47 +0530

    complete

--- JS 7 - Document Object Model part 2/script.js
+++ JS 7 - Document Object Model part 2/script.js
@@ -41,6 +41,48 @@ console.log("Farhan");
 
 // node.remove() : removes the node
 
-let newBtn = document.createElement("button");
-newBtn.innerText = "Click Me!";
-console.log(newBtn);
+// let newBtn = document.createElement("button");
+// newBtn.innerText = "Click Me!";
+// console.log(newBtn);
+
+// let div = document.querySelector("div");
+// div.append(newBtn);
+// div.prepend(newBtn);
+// div.before(newBtn);
+// div.after(newBtn);
+
+// let p = document.querySelector("p");
+// p.after(newBtn);
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = "<i>Hi, I am new.</i>";
+
+// document.querySelector("body").prepend(newHeading);
+
+// let para = document.querySelector("p");
+// para.remove();
+// newHeading.remove();
+
+// Qs : Create a button element. Give it a text "click me!", background color of red & text color of white.
+//      Insert the button as the first element inside the body tag.
+
+// let btn = document.createElement("button");
+// btn.innerText = "Click me!";
+// btn.style.color = "white";
+// btn.style.backgroundColor = "red";
+// document.querySelector("body").prepend(btn);
+
+// Qs : create a <p> tag in html, give it a class & some styling.
+//      Now create a new class in CSS and try to append this class to the <p> element.
+//      Did you noice, how you overwrite the class name when you add a new one?
+//      Solve this problem using classList
+
+// let para = document.querySelector("p");
+// para.getAttribute("class");
+// para.setAttribute("class", "newClass");     // drawback of using this is that earlier css won't be available.
+
+// para.classList.add("newClass");
+// para.classList.remove("newClass");
+
+// para.classList();        // will show how many classes are defined for the given tag
+

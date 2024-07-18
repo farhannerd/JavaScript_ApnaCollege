272dbeb993beab091bdca3f35c3229048021dbe6
Farhan Akhtar <gitfarhan@gmail.com>
2024-07-18 18:47:48 +0530

    hehehehe

--- JS 8 - Events/script.js
+++ JS 8 - Events/script.js
@@ -1,24 +1,33 @@
-console.log("Events");
+// console.log("Events");
 
-let btn1 = document.querySelector("#btn1");
+// let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () => {
-    console.log("hihihihihi");
-    let a = 25;
-    a++;
-    console.log(a);
-}
+// btn1.onclick = () => {
+//     console.log("hihihihihi");
+//     let a = 25;
+//     a++;
+//     console.log(a);
+// }
 
-btn1.onclick = (evt) => {
-    console.log(evt);
-    console.log(evt.type);
-    console.log(evt.target);
-    console.log(evt.clientX, evt.clientY);
+// btn1.onclick = (evt) => {
+//     console.log(evt);
+//     console.log(evt.type);
+//     console.log(evt.target);
+//     console.log(evt.clientX, evt.clientY);
 
-}
+// }
 
 
-let div = document.querySelector("div");
-div.onmouseover = () => {
-    div.style.color = "yellow";
-}
+// let div = document.querySelector("div");
+// div.onmouseover = () => {
+//     div.style.color = "yellow";
+// }
+
+// -------------------------------------------------------------------------
+// Evenet listeners
+
+// node.addEventListener(event, callback)
+// node.removeEventListener(event, callback)
+
+// Note : the callback reference should be same to remove
+

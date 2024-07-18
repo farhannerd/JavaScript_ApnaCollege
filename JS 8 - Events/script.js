3be74f03ebc8a86069c8dd8b8f94234b962aba3d
Farhan Akhtar <gitfarhan@gmail.com>
2024-07-18 18:39:17 +0530

    jojojojojojojojojojojo

---
+++ JS 8 - Events/script.js
@@ -0,0 +1,24 @@
+console.log("Events");
+
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () => {
+    console.log("hihihihihi");
+    let a = 25;
+    a++;
+    console.log(a);
+}
+
+btn1.onclick = (evt) => {
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
+
+}
+
+
+let div = document.querySelector("div");
+div.onmouseover = () => {
+    div.style.color = "yellow";
+}
